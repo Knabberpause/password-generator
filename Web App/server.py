1b79851a09bbd5103a493efca4eec32f2c3de9f6
Seth Bailey <104792463+Knabberpause@users.noreply.github.com>
2022-07-04 16:19:44 +0100

    Add files via upload

---
+++ Web App/server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask import *
+import random
+ 
+# WSGI Application
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+
+ 
+@app.route('/')
+def index():
+    length=random.randint(6,16)
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890():;,.-_=!@'
+
+    pwd1=""
+    pwd2=""
+    pwd3=""
+    pwd4=""
+    pwd5=""
+    for character in range(0,length):
+            passwordchar = random.choice(characters)
+            pwd1 += passwordchar
+
+    for character in range(0,length):
+            passwordchar = random.choice(characters)
+            pwd2 += passwordchar
+
+    for character in range(0,length):
+            passwordchar = random.choice(characters)
+            pwd3 += passwordchar
+
+    for character in range(0,length):
+            passwordchar = random.choice(characters)
+            pwd4 += passwordchar
+
+    for character in range(0,length):
+            passwordchar = random.choice(characters)
+            pwd5 += passwordchar
+    
+    return render_template('index.html', pwd1=pwd1,pwd2=pwd2, pwd3=pwd3, pwd4=pwd4, pwd5=pwd5)
+
+
+if __name__=='__main__':
+    app.run()
